0fb059e8abb3da30e0857494ae38119930f02383
I'm <625013913@qq.com>
2021-12-29 10:25:27 +0800

    feat: 引入tdesign资源

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import { setupStore } from "/@/store";
 // WindiCSS
 import 'virtual:windi.css';
 import Antd from 'ant-design-vue';
+import TDesign from 'tdesign-vue-next';
 import '@purge-icons/generated'
+
+// 引入组件库全局样式资源
+import 'tdesign-vue-next/es/style/index.css';
 
 if (import.meta.env.DEV) {
     import('ant-design-vue/dist/antd.less');
@@ -21,6 +25,8 @@ setupStore(app);
 app.use(router);
 
 app.use(Antd);
+
+app.use(TDesign);
 
 app.mount('#app');
 
