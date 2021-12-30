b2fa35d69a04fe753e651c59ec4061b3f30804be
I'm <625013913@qq.com>
2021-12-30 10:01:27 +0800

    fix: 去除登录授权

--- src/main.js
+++ src/main.js
@@ -9,8 +9,6 @@ import Antd from 'ant-design-vue';
 import TDesign from 'tdesign-vue-next';
 import '@purge-icons/generated'
 
-import { MessagePlugin } from 'tdesign-vue-next';
-
 import Keycloak from 'keycloak-js'
 // 引入组件库全局样式资源
 import 'tdesign-vue-next/es/style/index.css';
@@ -38,28 +36,4 @@ app.use(Antd);
 
 app.use(TDesign);
 
-keycloak.init({ onLoad: initOptions.onLoad }).then((auth) => {
-    if (!auth) {
-        // window.location.reload();
-    } else {
-        app.mount('#app');
-    }
-
-    // Token Refresh
-    setInterval(() => {
-        keycloak.updateToken(70).then((refreshed) => {
-            if (refreshed) {
-                console.info("🚀 ~ file: main.js ~ line 41 ~ keycloak.updateToken ~ Token refreshed", refreshed)
-            } else {
-                console.info("🚀 ~ file: main.js ~ line 45 ~ keycloak.updateToken ~ ", 'Token not refreshed, valid for '
-                    + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds')
-            }
-        }).catch(() => {
-            console.error("🚀 ~ file: main.js ~ line 44 ~ keycloak.updateToken ~ Failed to refresh token")
-        });
-    }, 6000)
-
-}).catch((error) => {
-    console.error("🚀 ~ file: main.js ~ line 52 ~ keycloak.init ~ Authenticated Failed", error)
-    MessagePlugin.error({ content: '身份认证失败', duration: 0 });
-});
+app.mount('#app');
